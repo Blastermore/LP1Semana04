2e154a2aa29577dab1142f6b843467123ab6ea6d
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-03-14 23:31:15 +0000

    Added general UI for the main method and print of the final result

--- Special/Program.cs
+++ Special/Program.cs
@@ -9,16 +9,19 @@ namespace Special
             int r;
             if (n == 0 || n == 1)
             {
-                r = n
+                r = n;
             }
             else
             {
                 r = GetSpecial(n - 1) + 2 * GetSpecial(n - 2);
             }
+            return r;
         } 
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        {   
+            Console.Write("Insert n-th number of the special sequence: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetSpecial(n));
         }
     }
 }
