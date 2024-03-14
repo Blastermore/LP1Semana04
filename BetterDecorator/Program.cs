c2732793429aee850eb63f8e15cb0c6c7434f0ae
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-03-14 22:20:28 +0000

    Added a loop to add the decor the number o times specified to each end of the string s

---
+++ BetterDecorator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BetterDecorator
+{
+    class Program
+    {
+        private static string Decor(string s, char dec, int x)
+        {
+            string final = $" {s} ";
+            for (int i = 1; i <= x; i++)
+            {
+                final = $"{dec}" + final + $"{dec}";
+            }
+            return final;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Decor("Good Morning!", '*', 3));
+            Console.WriteLine(Decor("Good Morning!", '.', 3));
+
+        }
+    }
+}
