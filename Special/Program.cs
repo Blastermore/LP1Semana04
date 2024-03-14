0b3d71cb0cca8ce720a70c8382e7952c8119acd1
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-03-14 23:27:23 +0000

    Added the general logic for the recursive method

---
+++ Special/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Special
+{
+    class Program
+    {
+        private static int GetSpecial(int n)
+        {
+            int r;
+            if (n == 0 || n == 1)
+            {
+                r = n
+            }
+            else
+            {
+                r = GetSpecial(n - 1) + 2 * GetSpecial(n - 2);
+            }
+        } 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
